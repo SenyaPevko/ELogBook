758897b0b00d4a7362d2f966849c9191ef6aa4c1
kozlov.danil <kozlov.danil@skbkontur.ru>
2025-05-09 14:25:33 +0500

    added a bit of access checker base logic

---
+++ Domain/AccessChecker/IAccessChecker.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.AccessChecker;
+
+public interface IAccessChecker<in TEntity> 
+    where TEntity : EntityInfo
+{
+    Task<bool?> CanRead();
+    Task<bool> CanRead(TEntity entity);
+
+    Task<bool?> CanUpdate();
+    Task<bool> CanUpdate(TEntity entity);
+
+    Task<bool?> CanCreate();
+    Task<bool> CanCreate(TEntity entity);
+}
---
+++ Infrastructure/AccessChecker/AccessCheckerBase.cs
@@ -0,0 +1,43 @@
+﻿using Domain.AccessChecker;
+using Domain.Entities.Base;
+using Domain.Entities.Roles;
+using Infrastructure.Context;
+
+namespace Infrastructure.AccessChecker;
+
+public class AccessCheckerBase<TEntity>(IRequestContext context) : IAccessChecker<TEntity>
+    where TEntity : EntityInfo
+{
+    public Task<bool?> CanRead()
+    {
+        if (context.Auth.Role is UserRole.Admin)
+            return true;
+        if (context.Auth.Role is UserRole.Unknown)
+            return null;
+    }
+
+    public Task<bool> CanRead(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool?> CanUpdate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CanUpdate(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool?> CanCreate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CanCreate(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
